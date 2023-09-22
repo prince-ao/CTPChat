df69137d1c636b11cb2e248f27c0d45d27464f45
prince-ao <prince.addo@cix.csi.cuny.edu>
2023-09-22 15:53:54 -0400

    frontend rebuild

---
+++ frontend/app/login/page.tsx
@@ -0,0 +1,91 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  email: z.string().min(2).email({ message: "Must enter a valid email" }),
+  password: z
+    .string()
+    .min(6, { message: "password length must be greater then 6" }),
+});
+
+export default function Login() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema as any),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+  return (
+    <main className="flex items-center justify-center min-h-[100vh]">
+      <Card className="w-[50%]">
+        <CardHeader>
+          <CardTitle>Login</CardTitle>
+        </CardHeader>
+        <CardContent className="">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input type="email" placeholder="email..." {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        placeholder="password..."
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit">Submit</Button>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </main>
+  );
+}
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,9 @@
+import Image from 'next/image'
+
 export default function Home() {
   return (
-    <main className="">
-      <h1>Hello, World</h1>
+    <main className=" bg-red-600 flex items-center justify-center w-[100vw] h-[100vh]">
+      <h1 className="text-3xl font-bold">Welcome to CTPChat</h1>
     </main>
-  );
+  )
 }
